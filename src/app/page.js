3335e05ea8d4262bb8d5bf2a1574b81d22cf5bac
Aavishkar Kolte <aavishkarkolte@gmail.com>
2024-12-28 16:32:38 +0530

    Added filter and limit features

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 "use client";
 import { useState, useEffect } from 'react';
-import { Container, Typography, Stack } from "@mui/material";
+import { Container, Typography, Stack, Select, MenuItem, FormControl, InputLabel, TextField, Pagination } from "@mui/material";
 import PaginationGrid from "@/components/PaginationGrid";
-
 
 export default function Home() {
   const [totalPokemonCount, setTotalPokemonCount] = useState(0);
@@ -10,9 +9,22 @@ export default function Home() {
   const [limit, setLimit] = useState(10);
   const [offset, setOffset] = useState(0);
   const [loading, setLoading] = useState(true);
+  const [types, setTypes] = useState([]);
+  const [selectedType, setSelectedType] = useState('');
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    const fetchPokemonData = async () => {
+    const fetchTypes = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/type');
+      const data = await response.json();
+      setTypes(data.results);
+    };
+
+    fetchTypes();
+  }, []);
+
+  useEffect(() => {
+    const fetchAllPokemon = async () => {
       setLoading(true);
       const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
       const response = await fetch(url);
@@ -34,17 +46,92 @@ export default function Home() {
       setLoading(false);
     };
 
-    fetchPokemonData();
-  }, [offset, limit]);
+    if (!selectedType) {
+      fetchAllPokemon();
+    }
+  }, [offset, limit, selectedType]);
+
+  useEffect(() => {
+    const fetchPokemonByType = async () => {
+      setLoading(true);
+      const url = `https://pokeapi.co/api/v2/type/${selectedType}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setTotalPokemonCount(data.pokemon.length);
+
+      const updatedPokemonList = await Promise.all(
+        data.pokemon.map(async ({ pokemon }) => {
+          const pokemonDetailsResponse = await fetch(pokemon.url);
+          const pokemonDetails = await pokemonDetailsResponse.json();
+          return {
+            ...pokemon,
+            imageUrl: pokemonDetails.sprites.front_default,
+          };
+        })
+      );
+
+      setPokemonList(updatedPokemonList);
+      setLoading(false);
+    };
+
+    if (selectedType) {
+      fetchPokemonByType();
+    }
+  }, [selectedType]);
+
+  const handleTypeChange = (event) => {
+    setSelectedType(event.target.value);
+    setOffset(0);
+  };
+
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+    setOffset(0);
+  };
+
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+    setOffset((value - 1) * limit);
+  };
+
+  const totalPages = Math.ceil(totalPokemonCount / limit);
 
   return (
     <>
       <Container>
         <Stack spacing={2}>
           <Typography variant="h4" sx={{ textAlign: 'center' }}>Pokemon Cards</Typography>
-          <PaginationGrid pokemonList={pokemonList} totalPokemonCount={totalPokemonCount} limit={limit} setOffset={setOffset} />
-        </Stack>
-      </Container>
+          <Container>
+            <Stack spacing={2} alignItems={"center"}>
+              <FormControl fullWidth>
+                <InputLabel>Type</InputLabel>
+                <Select value={selectedType} onChange={handleTypeChange}>
+                  <MenuItem value=""><em>None</em></MenuItem>
+                  {types.map((type) => (
+                    <MenuItem key={type.name} value={type.name}>{type.name}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <TextField
+                label="Limit"
+                type="number"
+                value={limit}
+                onChange={handleLimitChange}
+                fullWidth
+              />
+              <PaginationGrid
+                pokemonList={pokemonList}
+                totalPokemonCount={totalPokemonCount}
+                limit={limit}
+                setOffset={setOffset}
+                totalPages={totalPages}
+              />
+              <Pagination count={Math.ceil(totalPokemonCount / limit)} variant="outlined" shape="rounded" color="primary" page={page} onChange={handlePageChange} />
+            </Stack>
+          </Container>
+        </Stack >
+      </Container >
     </>
   );
 }
--- src/components/PaginationGrid.js
+++ src/components/PaginationGrid.js
@@ -1,35 +1,32 @@
-import { Card, Typography, CardMedia, CardContent, CardActions, Button, Stack, Container, Pagination, Grid2 } from "@mui/material";
+import { Card, Typography, CardMedia, CardContent, CardActions, Button, Stack, Container, Grid2 } from "@mui/material";
 
-export default function PaginationGrid({ children, pokemonList, totalPokemonCount, limit, setOffset }) {
+export default function PaginationGrid({ children, pokemonList }) {
     return (
         <Container>
-            <Stack spacing={2} alignItems="center">
-                <Grid2 container spacing={2}>
-                    {
-                        pokemonList.map((pokemon, index) => (
-                            <Grid2 key={index} xs={12} sm={6} md={4} lg={3}>
-                                <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                                    <CardMedia
-                                        component="img"
-                                        sx={{ height: 140, objectFit: 'contain' }}
-                                        image={pokemon.imageUrl}
-                                        title={pokemon.name}
-                                    />
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant="h5" component="div">
-                                            {pokemon.name}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small">Learn More</Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid2>
-                        ))
-                    }
-                </Grid2>
-                <Pagination count={Math.ceil(totalPokemonCount / limit)} variant="outlined" shape="rounded" color="primary" onChange={(e, page) => setOffset((page - 1) * limit)} />
-            </Stack>
+            <Grid2 container spacing={2}>
+                {
+                    pokemonList.map((pokemon, index) => (
+                        <Grid2 key={index} xs={12} sm={6} md={4} lg={3}>
+                            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                                <CardMedia
+                                    component="img"
+                                    sx={{ height: 140, objectFit: 'contain' }}
+                                    image={pokemon.imageUrl}
+                                    title={pokemon.name}
+                                />
+                                <CardContent sx={{ flexGrow: 1, height: 100 }}>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {pokemon.name}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid2>
+                    ))
+                }
+            </Grid2>
         </Container>
     );
 }
